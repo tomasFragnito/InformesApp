ef6d68c5ab211a740ec97e7e5fe546a472415cb9
tomasfrg <tomignacio2022@gmail.com>
2026-02-06 15:34:23 -0300

    cambio de nombres

--- frontend/infoApp-adminitrator/src/App.js
+++ frontend/infoApp-adminitrator/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import { Report } from "./components/Report";
 import { Navbar } from "./components/Navbar";
