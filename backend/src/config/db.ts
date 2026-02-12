6d903efd1ae4c619fb6076cde8097235cb1a3e69
tomasfrg <tomignacio2022@gmail.com>
2026-02-12 14:06:46 -0300

    actualizacion de sql

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(String(process.env.DB_SQL!),
-    {
-        dialect: "mysql",
-        logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-);
+  },
+  logging: false
+});
 
 export default sequelize;
