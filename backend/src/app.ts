4c821f4ea964c3145d1c0dcede5a8d661607988b
tomasfrg <tomignacio2022@gmail.com>
2026-02-12 15:52:41 -0300

    creacion de carpeta upload dinamicacmente

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express"; 
+import fs from "fs";
+import path from "path";
 import "./models";
 import sequelize from "./config/db";
 import cors from "cors";
@@ -22,6 +24,12 @@ app.use(cors({
 app.use(cors());
 
 app.use("/api", reportRoutes);
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 app.use((req, _res, next) => {
   console.log(req.method, req.path);
